23611d51f0fb7b6da9a976af8d78cc087167065f
swfs0417 <apdl0012@gmail.com>
2025-04-07 23:10:56 +0900

    solved

--- PS basic/04/07/어깨동무.py
+++ PS basic/04/07/어깨동무.py
@@ -1,27 +1,28 @@
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 
-l, r = 0, max(a)  # noqa: E741
-m = (l + r) // 2  # 점수차이
-# k: 목표 지친사람 수 (maximize k)
-# minimize m
-# counter = 0
-while l < r:
-    # counter += 1
-    tired = 0
-    for i in range(len(a)):
-        if (i != 0 and abs(a[i] - a[i - 1]) > m) or (
-            i != len(a) - 1 and abs(a[i] - a[i + 1]) > m
+
+def gettired(score):
+    acc = 0
+    for i in range(n):
+        if (i != 0 and abs(a[i] - a[i - 1]) > score) or (
+            i != len(a) - 1 and abs(a[i] - a[i + 1]) > score
         ):
-            tired += 1
-    if tired > k:
+            acc += 1
+    return acc
+
+
+l, r = 0, max(a)
+m = (l + r) // 2
+
+while l < r:
+    t = gettired(m)
+    # print(l, m, r, t)
+    if t > k:
         l = m if l != m else m + 1
         m = (l + r) // 2
-    elif tired < k:
+    elif t <= k:
         r = m
-        m = (l + r) // 2
-    else:
-        r -= 1
         m = (l + r) // 2
 
 print(m)
